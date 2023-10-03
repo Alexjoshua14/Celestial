568a0f4039b5434f659c0e046589700828ff9788
Alex <ajoshua14@icloud.com>
2023-10-02 20:47:22 -0700

    Add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface FooterProps {
+
+}
+
+const Footer: FC<FooterProps> = ({ }) => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-background-accent w-full h-80">
+      Footer
+    </div>
+  )
+}
+
+export default Footer
