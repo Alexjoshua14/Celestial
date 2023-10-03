3dfbc907b4252c7f2e9d70a1611b78154afb4436
Alex <ajoshua14@icloud.com>
2023-10-03 16:15:13 -0700

    Remove import ordering rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,3 @@
 module.exports = {
   extends: "next/core-web-vitals",
-  rules: {
-    "import/order": [
-      "error",
-      {
-        groups: [["builtin", "external"], ["internal", "parent", "sibling", "index"], ["unknown"]],
-        "newlines-between": "always",
-        pathGroups: [
-          {
-            pattern: '^(react$|next$|next/**)',
-            group: "external",
-            position: "before"
-          }
-        ],
-        "pathGroupsExcludedImportTypes": ["react$", "next$", "next/**"],
-        "alphabetize": {
-          "order": "asc",
-          "caseInsensitive": true
-        }
-      }
-    ]
-  }
 }
