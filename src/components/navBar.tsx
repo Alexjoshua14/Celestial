b887fc2953380e3fa154dc8abf09c1b645827ef5
Alex <ajoshua14@icloud.com>
2023-10-02 20:21:57 -0700

    Create NavBar

---
+++ src/components/navBar.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react'
+import { IoChevronDown } from 'react-icons/io5'
+
+interface NavBarProps extends React.HTMLAttributes<HTMLElement> {
+
+}
+
+/**
+ * 
+ * TODO: shadow should be present when navBar is overlayed
+ */
+const NavBar: FC<NavBarProps> = ({ ...props }) => {
+  return (
+    <section className="z-50 h-[4.5rem] w-full sticky top-0 backdrop-blur-2xl flex items-center justify-between px-8 py-4" {...props}>
+      <h1 className="text-primary-500 font-bold text-2xl">
+        celestial
+      </h1>
+      <div className="flex gap-8 text-lg font-light">
+        <p>
+          Login
+        </p>
+        <div className="flex items-center justify-center gap-1">
+          <p>
+            Menu
+          </p>
+          <IoChevronDown size={16} />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default NavBar
