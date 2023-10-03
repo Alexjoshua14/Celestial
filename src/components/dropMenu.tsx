e9090d902805c66b9ca858c480717efc401be7c9
Alex <ajoshua14@icloud.com>
2023-10-03 13:02:06 -0700

    Update DropMenu top section styling

--- src/components/dropMenu.tsx
+++ src/components/dropMenu.tsx
@@ -11,9 +11,9 @@ interface DropMenuProps {
 
 const DropMenu: FC<DropMenuProps> = ({ close }) => {
   return (
-    <div className="z-50 w-full sticky top-3 flex items-center justify-center transition-all">
-      <div className="w-[95%] max-h-[90vh] p-6 flex flex-col gap-8 backdrop-blur-2xl rounded-lg border-[1px] border-white shadow-md overflow-auto">
-        <div className="w-full flex justify-between">
+    <div className="z-40 w-full sticky top-3 flex items-center justify-center transition-all">
+      <div className="w-[95%] max-h-[90vh] relative backdrop-blur-2xl rounded-lg border-[1px] border-white shadow-md overflow-auto">
+        <div className="z-50 w-full h-full p-6 flex justify-between sticky top-0 bg-gradient-to-t to-background-primary from-transparent to-20%">
           <h1 className="title">
             Menu
           </h1>
@@ -21,7 +21,7 @@ const DropMenu: FC<DropMenuProps> = ({ close }) => {
             <IoClose size={24} className="title" />
           </button>
         </div>
-        <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6 p-6 pt-10">
           <Accordion type="single" collapsible className="flex flex-col gap-4 header">
             {dropMenuContent.map(section => (
               <DropMenuItem key={section.title} section={section} />
